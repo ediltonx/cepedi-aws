f230c8106d090e6e3b32da6e74bffde7f462e784
Edilton Jr <55814091+ediltonx@users.noreply.github.com>
2024-10-20 16:51:11 -0300

    adicionando script para automacao

---
+++ escripte-ed.sh
@@ -0,0 +1,20 @@
+# SCRIPT PARA AUTOMATIZAR OS PASSOS DA ATIVIDADE - EDILTON JR
+
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get --install-recommends install ca-certificates curl git -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Instalar o docker e etc...
+sudo apt-get --install-recommends  install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
